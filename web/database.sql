25310d98e1340b0e98f84065c88fa3b0f8cea655
aliceedn <alice.edn@gmail.com>
2017-03-14 14:22:00 +0100

    Initial commit

--- web/database.sql
+++ web/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 13 Mars 2017 à 15:59
+-- Généré le :  Mar 14 Mars 2017 à 14:21
 -- Version du serveur :  5.7.17-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.15-0ubuntu0.16.04.4
 
@@ -60,8 +60,73 @@ INSERT INTO `categories` (`id`, `Nom`) VALUES
 (1, 'sciencefiction'),
 (2, 'Horreur'),
 (3, 'super bouquin'),
-(4, 'toto'),
-(6, 'tartanpion');
+(4, 'totoleretour');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `copy`
+--
+
+CREATE TABLE `copy` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `etat_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `prix` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `copy`
+--
+
+INSERT INTO `copy` (`id`, `book_id`, `etat_id`, `status_id`, `prix`) VALUES
+(1, 1, 2, 3, '12'),
+(2, 2, 1, 4, '45'),
+(3, 2, 1, 3, '12'),
+(4, 2, 2, 4, '45');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etat`
+--
+
+CREATE TABLE `etat` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `etat`
+--
+
+INSERT INTO `etat` (`id`, `nom`) VALUES
+(1, 'neuf'),
+(2, 'occasion'),
+(3, 'detruit'),
+(4, 'pages déchirées');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `status`
+--
+
+INSERT INTO `status` (`id`, `nom`) VALUES
+(1, 'rayon'),
+(2, 'carton'),
+(3, 'vendu'),
+(4, 'emprunté');
 
 --
 -- Index pour les tables exportées
@@ -81,6 +146,27 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `copy`
+--
+ALTER TABLE `copy`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_4DBABB8216A2B381` (`book_id`),
+  ADD KEY `IDX_4DBABB82D5E86FF` (`etat_id`),
+  ADD KEY `IDX_4DBABB826BF700BD` (`status_id`);
+
+--
+-- Index pour la table `etat`
+--
+ALTER TABLE `etat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -93,7 +179,22 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT pour la table `copy`
+--
+ALTER TABLE `copy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `etat`
+--
+ALTER TABLE `etat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Contraintes pour les tables exportées
 --
@@ -103,6 +204,14 @@ ALTER TABLE `categories`
 --
 ALTER TABLE `books`
   ADD CONSTRAINT `FK_4A1B2A92BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`);
+
+--
+-- Contraintes pour la table `copy`
+--
+ALTER TABLE `copy`
+  ADD CONSTRAINT `FK_4DBABB8216A2B381` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `FK_4DBABB826BF700BD` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`),
+  ADD CONSTRAINT `FK_4DBABB82D5E86FF` FOREIGN KEY (`etat_id`) REFERENCES `etat` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
